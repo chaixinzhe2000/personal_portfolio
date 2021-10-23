9f28c0bcf1ad23e3015f73ec4dd04aa0a2fe8160
Xinzhe Chai <xinzhe_chai@brown.edu>
2021-10-23 17:34:42 -0400

    header finished

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,14 +3,23 @@ import React from 'react';
 /** @jsx jsx */
 import { css, jsx } from '@emotion/react'
 import * as styles from './styles'
+import Github from '../../images/github.svg'
+import Facebook from '../../images/fb.svg'
+import Email from '../../images/email.svg'
+import LinkedIn from '../../images/linkedin.svg'
 
 export function Header() {
 	return (
 		<div css={styles.headerWrapper}>
 			<div css={styles.content}>
-			<div css={styles.name}>Xinzhe Chai</div>
-			<div css={styles.tagline}>Software Developer</div>
-
+				<div css={styles.name}>Xinzhe Chai</div>
+				<div css={styles.tagline}>Software Developer</div>
+				<div css={styles.iconGroup}>
+					<img css={[styles.githubIcon, styles.icon]} src={Github} alt="github" />
+					<img css={[styles.icon, styles.fbIcon]} src={Facebook} alt="facebook" />
+					<img css={styles.icon} src={Email} alt="email" />
+					<img css={styles.icon} src={LinkedIn} alt="linkedin" />
+				</div>
 			</div>
 		</div>
 	);
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -9,12 +9,16 @@ export const headerWrapper = css`
 
 export const name = css`
 	color: ${red};
+	cursor: pointer;
+	font-weight: 600;
 	label: name;
 `
 
 export const tagline = css`
 	color: ${gray};
-	margin-top: -3px;
+	margin-top: 2px;
+	margin-left: -2px;
+	line-height: 0.95em;
 	label: tagline;
 `
 
@@ -22,4 +26,26 @@ export const content = css`
 	font-size: 26px;
 	margin: 25px 36px;
 	label: content;
+`
+
+export const iconGroup = css`
+	width: max-content;
+	display: flex;
+	gap: 15px;
+	margin-top: 10px;
+	label: icongroup;
+`
+
+export const icon = css`
+	height: 18px;
+	cursor: pointer;
+	label: icon;
+`
+
+export const fbIcon = css`
+margin-top: 0.5px;
+`
+
+export const githubIcon = css`
+margin-top: 1px;
 `
