32fb8402d01bcae1a7c1aaea8aa7a74679406d61
XInzhe Chai <xinzhe_chai@brown.edu>
2022-01-15 05:24:07 -0500

    added local fonts, loads faster

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -5,22 +5,21 @@ import { jsx } from '@emotion/react'
 import * as styles from './styles'
 
 export function Menu() {
-  const introText =
-    'I am a junior studying Computer Science at Brown University. At t' +
-    'he moment, I am an intern @ Zumper working on the frontend team, and I am passio' +
-    'nate about building exciting experience for everyone. As I keep on learning new ' +
-    'skills every day, I created this website to document my everyday projects and ex' +
-    'periences.'
+	const introText =
+		'I am a junior studying Computer Science at Brown University. I am passionate abo' +
+		'ut building exciting experience for everyone, through web technologies such as R' +
+		'eact, Redux, and Express. I am never afraid to take on engineering challenges wi' +
+		'th a learning mind, but I am probably drinking Aloe water while I tackle them.'
 
-  return (
-    <div css={styles.menu}>
-      <div css={styles.leftMenu}>
-        <div css={styles.menuItem}>Projects</div>
-        <div css={styles.menuItem}>Experiences</div>
-        <div css={styles.menuItem}>Education</div>
-        <div css={styles.menuItem}>Skills</div>
-      </div>
-      <div css={styles.intro}>{introText}</div>
-    </div>
-  )
+	return (
+		<div css={styles.menu}>
+			<div css={styles.leftMenu}>
+				<div css={styles.menuItem}>Projects</div>
+				<div css={styles.menuItem}>Experiences</div>
+				<div css={styles.menuItem}>Education</div>
+				<div css={styles.menuItem}>Skills</div>
+			</div>
+			<div css={styles.intro}>{introText}</div>
+		</div>
+	)
 }
